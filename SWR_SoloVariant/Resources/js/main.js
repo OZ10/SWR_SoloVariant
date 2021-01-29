e3cc0e0aa67446b72f080f4afbe817e0c5369084
OZ10 <mostin@gmail.com>
2021-01-29 11:22:30 +0000

    Added basic search option for Decision Card List

--- SWR_SoloVariant/Resources/js/main.js
+++ SWR_SoloVariant/Resources/js/main.js
@@ -1,7 +1,27 @@
 const f = document.getElementById('searchbutton');
 
-function run(){
-    alert('Working!');
+function run() {
+    // alert('Working?');
+
+    var searchText = document.getElementById('searchbar');
+
+    var collapseElementList = [].slice.call(document.querySelectorAll('.colTest'))
+
+    collapseElementList.forEach(row => {
+        if (row.title.includes(searchText.value)) {
+            row.classList.remove('d-none');
+            // var bsCol = new bootstrap.Collapse(row, {
+            //     show: true})
+        } else {
+            row.classList.add('d-none');
+            // row.collapse('show');
+        }
+    });
+
+    // var col = document.getElementById('rownum3');
+    // var bsCol = new bootstrap.Collapse(col, {
+    //     hide: true
+    // })
 }
 
 f.addEventListener('click', run);
