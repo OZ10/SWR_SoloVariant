907212a0427bf2442095649ee7f44d87426c9b8c
OZ10 <mostin@gmail.com>
2021-02-02 11:20:31 +0000

    Added Remote Systems button & basic modal display

--- SWR_SoloVariant/Resources/js/main.js
+++ SWR_SoloVariant/Resources/js/main.js
@@ -9,25 +9,17 @@ function run() {
     collapseElementList.forEach(row => {
         if (row.title.toUpperCase().includes(searchText.value.toUpperCase())) {
             row.classList.remove('d-none');
-            // var bsCol = new bootstrap.Collapse(row, {
-            //     show: true})
         } else {
             row.classList.add('d-none');
-            // row.collapse('show');
         }
     });
-
-    // var col = document.getElementById('rownum3');
-    // var bsCol = new bootstrap.Collapse(col, {
-    //     hide: true
-    // })
 }
 
-function run2(e){
-    if (e.keyCode == 13) {
-        run();
-    }
-}
+// function run2(e){
+//     if (e.keyCode == 13) {
+//         run();
+//     }
+// }
 
 // searchbutton.addEventListener('click', run);
 // searchbar.addEventListener('keydown', run2);
@@ -36,6 +28,22 @@ document.getElementById('searchbar').addEventListener('input', (e) => {
     run();
 })
 
-// run();
 
-// alert ('yo!');
+var numSelectedSystems = 0;
+
+document.querySelectorAll('.planetbtn').forEach(
+    planetbutton => {
+        planetbutton.addEventListener('click', (event) => {
+            if (planetbutton.checked == true) {
+                numSelectedSystems++
+            }else{
+                numSelectedSystems--
+            }
+            document.getElementById('numselectedsystems').innerText = numSelectedSystems;
+
+            if (numSelectedSystems == 7) {
+                alert('they be coming!');
+            }
+        })
+    }
+)
